7e9c60a6b7d77ac7501f8c482ed34d757f224396
Lazar-J <103836075+Lazar-J@users.noreply.github.com>
2023-03-29 11:48:40 +0200

    Make counter section

--- js/main.js
+++ js/main.js
@@ -2,10 +2,7 @@ window.onload = function() {
     const toggle = document.querySelector('#toggle-button');
     const navbar = document.querySelector('#hero-nav');
 
-    console.log(navbar);
-
     toggle.addEventListener('click', () => {
         navbar.classList.toggle('active')
-        console.log("click");
     });
 }
